e436d361316c4684946fa4d02790910e37fb82fa
Nicole Huang <nhuang@lcs.on.ca>
2021-10-26 16:41:15 -0400

    Finish the second Layout Exercise.

--- LayoutExercise/Views/ContentView.swift
+++ LayoutExercise/Views/ContentView.swift
@@ -9,7 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+            Text("ART")
+                .fontWeight(.black)
+                .font(.largeTitle)
+                
+            
+            
+        HStack{
+        
+        
+            Text("""
+With Manet's assistance, Monet found lodging in suburban Argenteuil in late 1871, a move that initiated one of the most fertile phases of his career. Impressionism evolved in the late 1860s from a desire to create full–scale, multi–figure depictions of ordinary people in casual outdoor situations. At its purest, impressionism was attuned to landscape painting, a subject Monet favored.
+
+Woman with a Parasol was painted outdoors, probably in a single session of several hours' duration. The artist intended the work to convey the feeling of a casual family outing rather than a formal portrait, and used pose and placement to suggest that his wife and son interrupted their stroll while he captured their likenesses.
+
+""")
+                .font(Font.custom("Text1", size: 12))
+        
+            VStack{
+                
+                Image("Art")
+                .resizable()
+                .frame(width: 135, height: 230)
+                
+                Spacer()
+                
+           Text("""
+In Woman with a Parasol – Madame Monet and Her Son, his skill as a figure painter is equally evident. Contrary to the artificial conventions of academic portraiture, Monet delineated the features of his sitters as freely as their surroundings. The spontaneity and naturalness of the resulting image were praised when it appeared in the second impressionist exhibition in 1876.
+""")
+        .font(Font.custom("text2", size: 12))
+        Spacer()
+            
+            
+            }
+            
+        }
+        }
             .padding()
     }
 }
---
+++ LayoutExercise/Views/secondLayoutExercise.swift
@@ -0,0 +1,68 @@
+//
+//  secondLayoutExercise.swift
+//  LayoutExercise
+//
+//  Created by Nicole on 2021/10/26.
+//
+
+import SwiftUI
+
+struct secondLayoutExercise: View {
+    var body: some View {
+        
+        HStack {
+            VStack {
+                HStack{
+                    
+                    Image("sky1")
+                        .resizable()
+                        .scaledToFit()
+                        
+                        
+                    VStack {
+                        Text("""
+            Why The Sky
+            is BLue
+            """)
+                            .fontWeight(.black)
+                        .font(.largeTitle)
+                        
+                        Divider()
+                    }
+                        Spacer()
+                        
+               
+                    
+                    
+                    
+                    
+                }
+                HStack{
+                    Text("As white light passes through our atmosphere, tiny air molecules cause it to 'scatter'. The scattering caused by these tiny air molecules (known as Rayleigh scattering) increases as the wavelength of light decreases. ... Therefore, blue light is scattered more than red light and the sky appears blue during the day.As white light passes through our atmosphere, tiny air molecules cause it to 'scatter'. The scattering caused by these tiny air molecules (known as Rayleigh scattering) increases as the wavelength of light decreases. ... Therefore, blue light is scattered more than red light and the sky appears blue during the day.")
+                    VStack{
+                        Text("As white light passes through our atmosphere, tiny air molecules cause it to 'scatter'. The scattering caused by these tiny air molecules (known as Rayleigh scattering) increases as the wavelength of light decreases. ... Therefore, blue light is scattered more than red light and the sky appears blue during the day.As white light passes through our atmosphere, tiny air molecules cause it to 'scatter'. The scattering caused by these tiny air molecules (known as Rayleigh scattering) increases as the wavelength of light decreases. ... Therefore, blue light is scattered more than red light and the sky appears blue during the day.")
+                        
+                        Image("sky1")
+                            .resizable()
+                            .scaledToFit()
+                        
+                        Text("Sunlight reaches Earth's atmosphere and is scattered in all directions by all the gases and particles in the air. Blue light is scattered more than the other colors because it travels as shorter, smaller waves. This is why we see a blue sky most of the time.")
+                      
+                            
+                        
+                    }
+                    Text("Sunlight reaches Earth's atmosphere and is scattered in all directions by all the gases and particles in the air. Blue light is scattered more than the other colors because it travels as shorter, smaller waves. This is why we see a blue sky most of the time.")
+                }
+                
+                Spacer()
+            }
+           
+        }
+    }
+}
+
+struct secondLayoutExercise_Previews: PreviewProvider {
+    static var previews: some View {
+        secondLayoutExercise()
+    }
+}
